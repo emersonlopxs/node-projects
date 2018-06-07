87d56017214ced25b3722263b55599c2e2209de1
lupuselit <lupuselit@outlook.com>
2018-06-07 11:55:37 -0300

    add node babel example

---
+++ node-babel/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { sum, minus } from './math';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('this is node with babel');
+});
+
+console.log(sum(1, 1));
+console.log(minus(1, 1));
+
+app.listen(3000);
---
+++ node-babel/math.js
@@ -0,0 +1,8 @@
+export function sum(a, b) {
+  const req = a + b;
+  return req;
+}
+
+export function minus(a, b) {
+  return a - b;
+}
